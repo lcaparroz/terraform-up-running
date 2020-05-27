3df4b69d14a6535019b95315f0c005c2f3edbf82
Luis Caparroz <lcaparroz.dev@gmail.com>
2020-05-27 09:48:45 -0300

    [Chp02] Add input/output vars to configuration

--- chp02/main.tf
+++ chp02/main.tf
@@ -2,6 +2,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
 resource "aws_instance" "example" {
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
@@ -10,7 +16,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, world!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   tags = {
@@ -18,12 +24,17 @@ resource "aws_instance" "example" {
   }
 }
 
+output "public_ip" {
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
