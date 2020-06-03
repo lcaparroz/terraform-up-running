daf785ff1a1558d8a0e11f9f1dcbd12456729e05
Luis Caparroz <lcaparroz.dev@gmail.com>
2020-06-02 21:01:06 -0300

    [Chp04] Add Terraform module outputs

--- chp04/modules/services/webserver-cluster/outputs.tf
+++ chp04/modules/services/webserver-cluster/outputs.tf
@@ -1,4 +1,9 @@
-output "aws_dns_name" {
+output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
+}
+
+output "asg_name" {
+  value       = aws_autoscaling_group.example.name
+  description = "The name of the Auto Scaling Group"
 }
